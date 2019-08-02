5be20351f7b7e2c3fa92e404a0b667c43744c8aa
Lantz <cnnrlantz@gmail.com>
2019-08-01 22:58:31 -0500

    BIG CHANGES PROBABLY BROKEN
    i updated also my pretty stuff

---
+++ car/Assets/Scenes/Aesthetic/Materials/CAieScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[ExecuteInEditMode]
+public class CAieScript : MonoBehaviour
+{
+    public Material Mat;
+    PostProcessVolume m_Volume;
+    ChromaticAberration m_ChromaticAbberation;
+
+    void Start()
+    {
+        m_ChromaticAbberation = ScriptableObject.CreateInstance<ChromaticAberration>();
+        m_ChromaticAbberation.enabled.Override(true);
+        m_ChromaticAbberation.intensity.Override(1f);
+
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_ChromaticAbberation);
+    }
+
+
+    private void Update()
+    {
+        m_ChromaticAbberation.intensity.value = Random.Range(0.5f, 1f);
+    }
+}
